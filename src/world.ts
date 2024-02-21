d3e425e2fbc87af2b3a2dd952b99e42f631c79b9
Nicolas Zulaica <nzulaica@fi.uba.ar>
2024-02-20 22:44:33 -0300

    circle collisions

---
+++ src/world.ts
@@ -0,0 +1,66 @@
+import Circle from "./circle";
+
+class World {
+  width: number;
+  height: number;
+  circles: Circle[] = [];
+
+  constructor(width, height, circles: Circle[] = []) {
+    this.width = width;
+    this.height = height;
+    this.circles = circles;
+  }
+
+  energy() {
+    let energy = 0;
+    for (let i = 0; i < this.circles.length; i++) {
+      energy += this.circles[i].energy();
+    }
+    return energy;
+  }
+
+  addCircle(circle: Circle) {
+    this.circles.push(circle);
+  }
+
+  checkCollisions() {
+    for (let i = 0; i < this.circles.length; i++) {
+      // Other Circles
+      for (let j = i + 1; j < this.circles.length; j++) {
+        if (this.circles[i].collidesWith(this.circles[j])) {
+          this.circles[i].collide(this.circles[j]);
+        }
+      }
+
+      // Walls
+      if (this.circles[i].x - this.circles[i].r < 0) {
+        this.circles[i].vx = Math.abs(this.circles[i].vx);
+      }
+      if (this.circles[i].x + this.circles[i].r > this.width) {
+        this.circles[i].vx = -Math.abs(this.circles[i].vx);
+      }
+      if (this.circles[i].y - this.circles[i].r < 0) {
+        this.circles[i].vy = Math.abs(this.circles[i].vy);
+      }
+      if (this.circles[i].y + this.circles[i].r > this.height) {
+        this.circles[i].vy = -Math.abs(this.circles[i].vy);
+      }
+    }
+  }
+
+  update(dt: number) {
+    this.checkCollisions();
+    for (let i = 0; i < this.circles.length; i++) {
+      this.circles[i].update(dt);
+    }
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.clearRect(0, 0, this.width, this.height);
+    for (let i = 0; i < this.circles.length; i++) {
+      this.circles[i].draw(ctx);
+    }
+  }
+}
+
+export default World;
